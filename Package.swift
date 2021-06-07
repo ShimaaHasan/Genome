312303c34154646767a3de142753b700e6e46ba1
Shimaa Hassa <78687204+ShimaaHasan@users.noreply.github.com>
2021-06-07 12:20:07 +0200

    update swift package to version 5.3

--- Package.swift
+++ Package.swift
@@ -1,31 +1,46 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Genome",
-    targets: [
-        Target(
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
             name: "Genome",
-            dependencies: [
-            ]
-        ),
-        Target(
-            name: "GenomeFoundation",
-            dependencies: [
-                .Target(name: "Genome")
-            ]
-        ),
-        Target(
-            name: "GenomeCoreData",
-            dependencies: [
-                .Target(name: "GenomeFoundation"),
-                .Target(name: "Genome")
-            ]
-        ),
+            targets: ["Genome"]),
+//        .library(
+//            name: "GenomeFoundation",
+//            targets: ["GenomeFoundation"]),
+//        .library(
+//            name: "GenomeCoreData",
+//            targets: ["GenomeCoreData"]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/node.git", majorVersion: 1)
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
-    exclude: [
-        "Sources/Genome/CoreData"
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Genome",
+            dependencies: [],
+            path: "Sources",
+            exclude: [
+                "Sources/Genome/CoreData",
+                "GCDWebServer.podspec",
+                "README.md",
+                "LICENSE"
+            ]),
+//        .target(
+//            name: "GenomeFoundation",
+//            dependencies: ["Genome"],
+//            path: "Sources"),
+//        .target(
+//            name: "GenomeCoreData",
+//            dependencies: ["Genome"],
+//            path: "Sources"),
     ]
 )
